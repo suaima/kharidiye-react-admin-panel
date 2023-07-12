a8c1a1ef65e72c2b3a5127903250ed14df2de439
Sumit Thakur <sumit.thakur@dotsquares.com>
2023-07-12 19:19:17 +0530

    Added CSS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Layout2 from "./components/Layout2";
 function App() {
   const currentPath = window.location.pathname;
 
-  console.log("currentPath", currentPath); // Log the current path
   return (
     <>
       {
@@ -18,31 +17,8 @@ function App() {
             <Layout2 />
           )
       }
-
-      {AddLibrary('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js')}
-      {AddLibrary("assets/plugins/jquery/jquery-3.5.1.min.js")}
-      {AddLibrary("assets/js/bootstrap.bundle.min.js")}
-      {AddLibrary("assets/plugins/simplebar/simplebar.min.js")}
-      {AddLibrary("assets/plugins/jquery-zoom/jquery.zoom.min.js")}
-      {AddLibrary("assets/plugins/slick/slick.min.js")}
-      {AddLibrary("assets/plugins/charts/Chart.min.js")}
-      {AddLibrary("assets/js/chart.js")}
-      {AddLibrary("assets/plugins/charts/google-map-loader.js")}
-      {AddLibrary("assets/plugins/charts/google-map.js")}
-      {AddLibrary("assets/plugins/daterangepicker/moment.min.js")}
-      {AddLibrary("assets/plugins/daterangepicker/daterangepicker.js")}
-      {AddLibrary("assets/js/date-range.js")}
-      {AddLibrary("assets/plugins/options-sidebar/optionswitcher.js")}
-      {AddLibrary("assets/js/admin.js")}
     </>
   );
-}
-// Create the function
-export function AddLibrary(urlOfTheLibrary) {
-  const script = document.createElement('script');
-  script.src = urlOfTheLibrary;
-  script.async = true;
-  document.body.appendChild(script);
 }
 
 export default App;
---
+++ src/context/index.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const AppContext = createContext({
+  sidebarMinified: "sidebar-minified-out",
+});
+
+export { AppContext };
