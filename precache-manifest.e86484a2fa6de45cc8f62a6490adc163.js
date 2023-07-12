1f64cdde8626bbc973a7c4c8ec0f045122c46bd8
sumitt806 <sumitt806@gmail.com>
2023-07-12 08:49:56 +0530

    Updates

---
+++ precache-manifest.e86484a2fa6de45cc8f62a6490adc163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf70f3f1593796a667868269758d56c7",
+    "url": "/kharidiye-react-admin-panel/index.html"
+  },
+  {
+    "revision": "47b386d0997bcc59f17a",
+    "url": "/kharidiye-react-admin-panel/static/css/main.f8863dd4.chunk.css"
+  },
+  {
+    "revision": "46f2420dd2df4f666a9b",
+    "url": "/kharidiye-react-admin-panel/static/js/2.1e92ed8f.chunk.js"
+  },
+  {
+    "revision": "4f7c94ddddfa775cb969607229d37707",
+    "url": "/kharidiye-react-admin-panel/static/js/2.1e92ed8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b386d0997bcc59f17a",
+    "url": "/kharidiye-react-admin-panel/static/js/main.2def4e61.chunk.js"
+  },
+  {
+    "revision": "fcb138a60e5909d8abd9",
+    "url": "/kharidiye-react-admin-panel/static/js/runtime-main.23d51c9b.js"
+  }
+]);
